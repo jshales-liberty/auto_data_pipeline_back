2a9b23cc8bf985ba64439806d03b85ffcaaef48b
Shales <n0236074@lm.lmig.com>
2017-07-19 14:41:25 -0400

    Cors

--- src/main/java/com/autolocations/auto_location/LocationDB.java
+++ src/main/java/com/autolocations/auto_location/LocationDB.java
@@ -21,8 +21,6 @@ public class LocationDB {
 		Connection conn = getConnection();
 		PreparedStatement pstmt_validate = conn
 				.prepareStatement("Select count(*) from vehlocation");
-		// pstmt_validate.setString(1, u.getUsername());
-		// pstmt_validate.setString(2, u.getEmail());
 		ResultSet rs = pstmt_validate.executeQuery();
 		rs.next();
 		return rs.getInt("count");
@@ -51,27 +49,3 @@ public class LocationDB {
 
 	}
 }
-
-// public Student getStudent(Integer id) {
-// String SQL = "select * from Student where id = ?";
-// Student student = jdbcTemplateObject.queryForObject(SQL,
-// new Object[]{id}, new StudentMapper());
-//
-// return student;
-// }
-// public List<Student> listStudents() {
-// String SQL = "select * from Student";
-// List <Student> students = jdbcTemplateObject.query(SQL, new StudentMapper());
-// return students;
-// }
-// public void delete(Integer id) {
-// String SQL = "delete from Student where id = ?";
-// jdbcTemplateObject.update(SQL, id);
-// System.out.println("Deleted Record with ID = " + id );
-// return;
-// }
-// public void update(Integer id, Integer age){
-// String SQL = "update Student set age = ? where id = ?";
-// jdbcTemplateObject.update(SQL, age, id);
-// System.out.println("Updated Record with ID = " + id );
-// return;
--- src/main/java/com/autolocations/auto_location/R_Controller.java
+++ src/main/java/com/autolocations/auto_location/R_Controller.java
@@ -6,6 +6,7 @@ import java.net.URISyntaxException;
 import java.sql.SQLException;
 import java.util.List;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,7 +16,7 @@ import com.google.gson.stream.JsonReader;
 
 @RestController
 public class R_Controller {
-
+	@CrossOrigin(origins = "http://localhost:4200")
 	@RequestMapping(path = "/api/testdata", method = RequestMethod.GET)
 	public Location[] grabdata() {
 		JsonReader reader;
@@ -28,13 +29,13 @@ public class R_Controller {
 			return null;
 		}
 	}
-
+	@CrossOrigin(origins = "http://localhost:4200")
 	@RequestMapping(path = "/api/testdata2", method = RequestMethod.GET)
 	public int grabtest() throws URISyntaxException, SQLException {
 		return LocationDB.testpull();
 
 	}
-
+	@CrossOrigin(origins = "http://localhost:4200")
 	@RequestMapping(path = "/api/updatelocations", method = RequestMethod.GET)
 	public List<Location> getLocs() throws URISyntaxException, SQLException {
 		return LocationDB.getCurrentLocations();
