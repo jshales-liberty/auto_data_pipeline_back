f8aa2d0e166c16b4643ba624bc86db8b21c7df3d
Gray <n0220007@lm.lmig.com>
2017-07-20 15:37:42 -0400

    something

--- src/main/java/com/autolocations/auto_location/UserDB.java
+++ src/main/java/com/autolocations/auto_location/UserDB.java
@@ -42,7 +42,7 @@ public class UserDB {
 			pstmt.setString(1, u.getEmail());
 			pstmt.setString(2, u.getPassword_hash());
 			ResultSet rs = pstmt.executeQuery();
-			rs.next();
+//			rs.next();
 //			if (rs.getInt("count") != 1) {
 				return rs.getInt("count");
 //			} else {
