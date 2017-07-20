9999177fafe026c0cbdecf6a84732a3548a424a2
Gray <n0220007@lm.lmig.com>
2017-07-20 16:13:01 -0400

    something

--- src/main/java/com/autolocations/auto_location/UserDB.java
+++ src/main/java/com/autolocations/auto_location/UserDB.java
@@ -38,7 +38,7 @@ public class UserDB {
 	public static String validateUser(AppUser u) throws URISyntaxException, SQLException {
 		try (Connection conn = getConnection();
 				PreparedStatement pstmt = conn.prepareStatement(
-						"select count(*) as count from users where email = '?';")) {
+						"select * from users where email = '?';")) {
 //						"select * from users where email = 'herbert@ttt.com';")) {
 			pstmt.setString(1, u.getEmail());
 //			pstmt.setString(2, u.getPassword_hash());
