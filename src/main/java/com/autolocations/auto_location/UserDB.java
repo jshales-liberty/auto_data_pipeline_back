802b1d383baeb4b11b9a60ec49ebee894cbe66d8
Gray <n0220007@lm.lmig.com>
2017-07-20 15:32:56 -0400

    remove public from user validation route

--- src/main/java/com/autolocations/auto_location/UserDB.java
+++ src/main/java/com/autolocations/auto_location/UserDB.java
@@ -38,7 +38,7 @@ public class UserDB {
 	public static int validateUser(AppUser u) throws URISyntaxException, SQLException {
 		try (Connection conn = getConnection();
 				PreparedStatement pstmt = conn.prepareStatement(
-						"select count(*) as count from public.users where email = ? and password_hash = ?;")) {
+						"select count(*) as count from users where email = ? and password_hash = ?;")) {
 			pstmt.setString(1, u.getEmail());
 			pstmt.setString(2, u.getPassword_hash());
 			ResultSet rs = pstmt.executeQuery();
