9b71490b33d1807dfea0808a1a36447826e984ce
Shales <n0236074@lm.lmig.com>
2017-07-26 11:51:07 -0400

    testing mode

--- src/main/java/com/autolocations/auto_location/LocationDB.java
+++ src/main/java/com/autolocations/auto_location/LocationDB.java
@@ -167,7 +167,7 @@ public class LocationDB {
 		try (Connection conn = getConnection();
 				PreparedStatement pstmt = conn.prepareStatement(
 						"Select t1.Lati, t1.Longi, t1.Status, t1.vid, t1.timestamp "
-								+ "from vehlocation t1 where ? "
+								+ "from vehlocation t1 where timestamp < extract(epoch from now()) "
 								+ "ORDER BY vid, timestamp ASC;");){
 						//ResultSet.TYPE_SCROLL_INSENSITIVE,
 						//ResultSet.CONCUR_UPDATABLE);) {
@@ -181,7 +181,7 @@ public class LocationDB {
 				sql_tweak += Integer.toString(endTime);
 			}
 
-			pstmt.setString(1, sql_tweak);
+			//pstmt.setString(1, sql_tweak);
 			ResultSet rs = pstmt.executeQuery();
 			List<Location> locations = new ArrayList<Location>();
 			double prev_lati = 0;
